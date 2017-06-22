93bf8dc3122a6c2337d1b3988070f96095143316
Hack Reactor Student <student@hackreactor.com>
2017-06-22 12:30:26 -0500

    Started thursday-AM lesson, completed letters a, f, n, e, i, s, l, w, g, q, o

--- practice.js
+++ practice.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
   // your code goes below
-
+  console.log("the page is loaded");
+  $("textarea").keypress(function(event){
+  // what to do when a key is pressed
+  var letter = event.key;    
+  console.log(letter);
+  })
 })
--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
   // your code goes below
+  console.log("Page loaded")
+  $("div").hide();
+  $("textarea").keypress(function(event){
+    console.log("a key was pressed!");
+    console.log(event.keyCode);
+    if (event.which == 97 || event.which == 65){ //a is pressed
+      $("body").css("background-color", "darkgreen");
+    } else if(event.which == 102 || event.which == 70){ // f is pressed
+      $("h1").css("color", "white");
+    } else if(event.which == 112 || event.which == 80){ // p is pressed
+      $("div").show();
+    } else if(event.which == 110 || event.which == 78){ // n is pressed
+      $("body").fadeOut(500);
+      $("body").fadeIn(500);
+    } else if(event.which == 101|| event.which == 69){ // e is pressed
+      $("body").append("<p>hello, is anyone there</p>");
+    } else if(event.which == 105 || event.which == 73){ // i is pressed
+      var currentPos = $("textarea").position();
+      $("textarea").css("position", "relative");
+      $("textarea").css("left", currentPos.left + 80 + "px");
+    } else if(event.which == 115 || event.which == 83){ // s is pressed
+      $("h1").text("I'M TRAPPED")
+    } else if(event.which == 108 || event.which == 76){ // l is pressed
+      $("body").css("background-image", "url(images/scaryclown.jpg)");
+    }
+  });
 })

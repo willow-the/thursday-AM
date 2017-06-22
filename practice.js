a4e6f93c8170374e696c3b992cc39ad4f9771899
Hack Reactor Student <student@hackreactor.com>
2017-06-22 12:31:26 -0500

    else if set up

--- practice.js
+++ practice.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
   // your code goes below
+  console.log("the page has loaded")
+  $("textarea").keypress(function(event){
+    console.log("key pressed in textarea")
+    var letter = event.key
+    console.log(letter);
+  })
 
 })
--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
   // your code goes below
+  console.log("hi")
+  $("div").hide()
+  $("textarea").keypress(function(event){
+    var letter = event.key
+    if (letter==="a") {
+      $("body").css("background-color","darkGreen")
+    }else if(letter==="f"){
+      $("h1").css("color","white")
+    }else if(letter==="p"){
+      $("div").show()
+    }else if(letter==="n"){
+      $("body").fadeout("slow")
+      $("body").fadein("slow")
+    }else if(letter==="e"){
+      $("#d").text("hello, is anyone there?")
+    }else if(letter==="i"){
+      $("textarea").css("margin-left","80px")
+    }else if(letter==="s"){
+      $("h1").text("I'M TRAPPED")
+    }else if(letter==="l"){
+      $("body").css("background-image","url(Images/scarryclown.jpg)")
+    }else if(letter==="w"){
+      $("#d").append("Would you like to play a game")
+    }else if(letter==="g"){
+      $("h3").prepend("HELP ME!!!")
+    }else if(letter==="q"){
+      $("textarea").text()
+    }else if(letter==="o"){
+      $("#body").css("background-image","none")
+    }
+  })
 })

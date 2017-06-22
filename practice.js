5b26bddfeae903d0b01c1fc239f42903edeb2e27
Hack Reactor Student <student@hackreactor.com>
2017-06-22 12:30:16 -0500

    Keypress

--- practice.js
+++ practice.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
   // your code goes below
+  console.log("the page is loaded");
+  $("textarea").keypress(function(event){
+    var letter = event.key; 
+    console.log(letter);
+  })
 
 })
--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
   // your code goes below
+  console.log("bob");
+  $("div").hide()
+  $("textarea").keypress(function(event){
+    var letter = event.key;
+    if(letter == "a"){
+     $("body").css("background-color", "#006400")
+    }else if(letter == "f"){
+     $("h1").css("color", "white") 
+    }else if(letter == "p"){
+      $("div").show()    
+    }else if(letter == "n"){
+      $("body").fadeOut("slow")
+      $("body").fadeIn("slow")
+    }else if(letter == "e"){
+      $("body").append("<p>hello, is anyone there?</p>")
+    }else if(letter == "i"){
+      $("textarea").css("right", "80px")
+    }else if(letter == "s"){
+      $("h1").text("I'M TRAPPED")
+    }
+  })
 })

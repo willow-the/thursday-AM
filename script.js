412a854d52f2a9ea36d3406514a71b5d73a5095b
Hack Reactor Student <student@hackreactor.com>
2017-06-22 12:30:24 -0500

    GHOST WRITER

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   // your code goes below
+  $(".dare").hide() 
+  $(".text").keypress(function(event) {
+ if (".text").keypress(key == "a") {
+    alert("an a was pressed")
+ } else { }
+  })
+  
+  
+  
 })

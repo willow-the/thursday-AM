2ffb2afc5cbe6816f14f3fe3020c40644e823653
Hack Reactor Student <student@hackreactor.com>
2017-06-23 12:10:15 -0500

    Ghost Writer

--- practice.js
+++ practice.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
   // your code goes below
-
+$("textarea").on("keypress", function(event) {
+  console.log(event.key);
+})
 })
--- script.js
+++ script.js
@@ -1,3 +1,53 @@
 $(document).ready(function(){
-  // your code goes below
+  $("#click").hide()
+console.log("Hi dudes")
+ 
+ $("textarea").on("keypress", function(event) {
+  if (event.key == "a") {
+    $("body").css("background-color", "darkgreen")
+}
+  else if (event.key == "f") {
+    $("h1").css("color", "white")
+  }
+  else if (event.key == "p") {
+    $("#click").show()
+  }
+  else if (event.key == "n") {
+    $("body").fadeOut(2000)
+    $("body").fadeIn(2000)
+  }
+  else if (event.key == "e") {
+    $("body").append("<p>Hello, is anyone there?</p>")
+  }
+  else if (event.key == "i") {
+    $("textarea").css("transform", "translateX(300px)")  
+  }
+  else if (event.key == "s") {
+    $("h1").replaceWith("<h1>Im Trapped!!!</h1>") 
+  }
+  else if (event.key == "l") {
+    $("body").css("background-image" , "url('images/scaryclown.jpg')")
+  }
+   else if (event.key == "w") {
+    $("body").append("<p>Would you like to play a game?</p>")
+  }
+  else if (event.key == "g") {
+    $("body").prepend("<h3>Help Me!!!!!!</h3>")
+  }
+  else if (event.key == "q") {
+    $("textarea").val("")
+  }
+  else if (event.key == "o") {
+    $("body").css("background-image", "unset")
+  }
+  $("#scary").on("click" , function(){
+    console.log("clicked")
+    $("body").css("background-color", "black")
+    $("textarea").hide()
+    $("body").append("<h1 id = 'escape'>You'll Never Escape!!!! <br> HA HA HA</h1>")
+    
 })
+})
+  
+})
+

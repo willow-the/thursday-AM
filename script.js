8be19d6cb0c75d477818530d6c1e9f5854e87b2f
Hack Reactor Student <student@hackreactor.com>
2017-06-23 12:18:46 -0500

    scary clown project

--- script.js
+++ script.js
@@ -23,7 +23,33 @@ $("textarea").keypress(function(event){
     $("body").fadeOut(3000)
     $("body").fadeIn(3000)
   
-}
+  }
   
+  else if(event.key=="e"){
+    $("body").append("Is anyone there...");
+  }
+
+  else if(event.key=="i"){
+    $("textarea").css("transform","translateX(80px)");
+  }
+
+  else if(event.key=="s"){
+    $("h1").text("I'M TRAPPED");
+  }
+
+  else if(event.key=="l"){
+    $("body").css("background-image","url('https://ericlahti.files.wordpress.com/2016/10/clowns-1.jpg')");}
+
+  else if(event.key=="w"){
+    $("body").prepend("<h3>HELP ME!!!!</h3>");
+}
+  else if(event.key=="q"){
+    $("textarea").val("")
+  }
+
+  else if(event.key=="o"){
+    $("body").css("background", "white")
+  }
+
 })
 })

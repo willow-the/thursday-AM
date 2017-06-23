4b4d9826df9125be1ffe952afec079d29233b94d
Hack Reactor Student <student@hackreactor.com>
2017-06-23 11:13:11 -0500

    worked on ghost writer

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-  // your code goes below
+
+$("textarea").on("keypress", function(event){
+console.log(event.key);
+})
+
+$("#clickIfYouDare").hide();
+
+$("textarea").keypress(function(event){
+  console.log("a key was pressed!")
+  if(event.key=="a"){
+    $("body").css("background-color", "#11561c")
+  }
+  else if(event.key=="f"){
+    $("h1").css("color", "white")
+  }
+
+  else if(event.key=="p"){
+    $("#clickIfYouDare").show();
+  }
+
+  else if(event.key=="n"){
+    $("body").fadeOut(3000)
+    $("body").fadeIn(3000)
+  
+}
+  
+})
 })
